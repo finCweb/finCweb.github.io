72e8efbe014a57a816f734e7563247c04bff9752
SemwalShaurya <semwalshaurya@gmail.com>
2021-12-02 00:06:19 +0530

    Added javascript file

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["ROORKEE", "₹OO₹K€€"],
+        typeSpeed: 150,
+        backSpeed: 100,
+        loop: true
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
